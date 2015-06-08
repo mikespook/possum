014b51a87360525f58e649d25f3fb4fc665d718d
Xing Xing <mikespook@gmail.com>
2015-06-08 13:08:11 +0800

    adding some helper functions

---
+++ helper.go
@@ -0,0 +1,44 @@
+package possum
+
+import (
+	"math/rand"
+	"net/http"
+)
+
+// Method takes one map as a paramater.
+// Keys of this map are HTTP method mapping to HandlerFunc(s).
+func Method(m map[string]HandlerFunc) HandlerFunc {
+	f := func(ctx *Context) error {
+		h, ok := m[ctx.Request.Method]
+		if ok {
+			return h(ctx)
+		}
+		ctx.Response.Status = http.StatusMethodNotAllowed
+		return nil
+	}
+	return f
+}
+
+// Chain combins a slide of HandlerFunc(s) in to one request.
+func Chain(h ...HandlerFunc) HandlerFunc {
+	f := func(ctx *Context) error {
+		for _, v := range h {
+			if err := v(ctx); err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+	return f
+}
+
+// Rand picks one HandlerFunc(s) in the slide.
+func Rand(h ...HandlerFunc) HandlerFunc {
+	f := func(ctx *Context) error {
+		if err := h[rand.Intn(len(h))](ctx); err != nil {
+			return err
+		}
+		return nil
+	}
+	return f
+}
