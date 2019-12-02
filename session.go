2f864a0b67617629fe628ffe63ef28f8cf1b74a9
Xing Xing <mikespook@gmail.com>
2019-12-02 16:47:11 +1300

    Change API of session

--- session.go
+++ session.go
@@ -1,11 +1,33 @@
 package possum
 
-import "github.com/mikespook/possum/session"
+import (
+	"context"
+	"net/http"
 
-// StartSession initaillizes a session context.
-// This function should be called in a implementation
-// of possum.HandleFunc.
-func (ctx *Context) StartSession(f session.FactoryFunc) (err error) {
-	ctx.Session, err = f(ctx.Response, ctx.Request)
-	return
+	"github.com/mikespook/possum/session"
+)
+
+// SessionName is the type of name for context
+type SessionName string
+
+const sessionCookieName = "possum"
+const sessionName SessionName = "session"
+
+// SessionFacotryFunc is the default factory function of session.
+var SessionFacotryFunc session.FactoryFunc = session.NewFactory(session.CookieStorage(sessionCookieName, nil))
+
+// Session extracts data from the request and returns session instance.
+// It uses SessionFacotryFunc to initialise session if no session has been set yet.
+func Session(w http.ResponseWriter, req *http.Request) (session *session.Session, err error) {
+	var ok bool
+	if session, ok = req.Context().Value(sessionName).(*Session); ok {
+		return session, nil
+	}
+
+	session, err = SessionFacotryFunc(w, req)
+	if err != nil {
+		return nil, err
+	}
+	context.WithValue(req.Context(), sessionName, session)
+	return session, nil
 }
